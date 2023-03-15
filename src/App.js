807e35485343500cffc36b58416e023420cbac69
VishalBhuse <vbhuse1997@gmail.com>
2023-03-15 16:12:14 +0530

    strutue

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Birthday from "./Pages/Birthday";
+import Home from "./Pages/Home";
+import OurProducts from "./Pages/OurProducts";
+import Travel from "./Pages/Travel";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <OurProducts />
+      <Birthday />
+      <Travel />
+    </>
+  );
+}
+
+export default App;

68f147ea86e4f825d73eaa210995eb7ec9fea21e
VishalBhuse <vbhuse1997@gmail.com>
2023-03-15 16:25:51 +0530

    strutue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Birthday from "./Pages/Birthday";
-import Home from "./Pages/Home";
+// import Birthday from "./Pages/Birthday";
+// import Home from "./Pages/Home";
 import OurProducts from "./Pages/OurProducts";
-import Travel from "./Pages/Travel";
+// import Travel from "./Pages/Travel";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
       <OurProducts />
-      <Birthday />
-      <Travel />
+      {/* <Birthday />
+      <Travel /> */}
     </>
   );
 }
--- src/Pages/OurProducts.jsx
+++ src/Pages/OurProducts.jsx
@@ -1,7 +1,32 @@
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const OurProducts = () => {
-  return <div>OurProducts</div>;
+  return (
+    <section>
+      <Box w={"90%"} mx="auto" backgroundColor={'red'}>
+        <Text
+          lineHeight={"42px"}
+          fontSize={"26px"}
+          textAlign="center"
+          fontFamily={"Lucida Bright"}
+          fontWeight="600"
+        >
+          Sail With Us
+        </Text>
+        <Text
+        fontWeight={400}
+          fontSize={"22px"}
+          lineHeight={"27px"}
+          textAlign="center"
+          fontFamily="Inter"
+        >
+          -Our Products-
+        </Text>
+      </Box>
+      <Box w='90%'h={'5px'} my="5px"></Box>
+    </section>
+  );
 };
 
 export default OurProducts;
